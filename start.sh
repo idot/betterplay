1dfa135ebe83c9001b2c4caf4dc1707651b40542
Ido Tamir <tamir@imp.ac.at>
2016-05-17 09:50:08 +0200

    added menu

--- start.sh
+++ start.sh
@@ -2,8 +2,9 @@
 
 module load oracle-jdk/1.8.0_72
 
-activator clean stage
+#activator clean stage
 #./target/universal/stage/bin/betterplay -Dconfig.file=./conf/production.conf -Dhttp.port=9055
-./target/universal/stage/bin/betterplay -Dhttp.port=9055
+#./target/universal/stage/bin/betterplay -Dhttp.port=9055
 
+bin/betterplay  -Dhttp.port=9055
 
---
+++ ui/src/app/components/menu/menu.directive.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ui')
+    .directive('betterMenuView', betterMenuView);
+
+  /** @ngInject */
+  function betterMenuView() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/menu/menu.html',
+      scope: {
+      },
+      controller: MenuViewController,
+      controllerAs: 'vm'
+    };
+
+    return directive;
+
+     /** @ngInject */
+    function MenuViewController($log, $scope, betterSettings , userService) {
+        var vm = this;
+        var DF = betterSettings.DF;
+               
+    } 
+  }
+
+})();
