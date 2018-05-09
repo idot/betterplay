7dd2b02d27f38a237a4bfa47c54df32e6e3e9bcc
Ido Tamir <ido.tamir@vbcf.ac.at>
2018-05-09 20:05:11 +0200

    getting somewhere

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,31 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule }   from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-import {MatIconModule} from '@angular/material/icon';
-import {DomSanitizer} from '@angular/platform-browser';
-import {MatIconRegistry, MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatIconModule} from '@angular/material/icon'
+import {DomSanitizer} from '@angular/platform-browser'
+import {MatIconRegistry, MatButtonModule, MatCheckboxModule} from '@angular/material'
 
 
-import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger'
 
-import { httpInterceptorProviders } from './http-interceptors';
-import { AppRoutingModule } from './app-routing.module';
+import { httpInterceptorProviders } from './http-interceptors'
+import { AppRoutingModule } from './app-routing.module'
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { UserBetsComponent } from './user-bets/user-bets.component';
+import { AppComponent } from './app.component'
+import { LoginComponent } from './pages/login/login.component'
+import { UserBetsComponent } from './pages/user-bets/user-bets.component'
 
-import { BetterdbService } from 'src/app/betterdb.service';
-import { UserService } from './user/user.service';
-import { BetViewComponent } from './components/bet-view/bet-view.component';
-import { GameViewComponent } from './components/game-view/game-view.component';
-import { UserViewComponent } from './components/user-view/user-view.component';
-import { FilterViewComponent } from './components/filter-view/filter-view.component';
-import { MenuComponent } from './components/menu/menu.component';
+import { BetterdbService } from 'src/app/betterdb.service'
+import { UserService } from './user/user.service'
+import { BetViewComponent } from './components/bet-view/bet-view.component'
+import { GameViewComponent } from './components/game-view/game-view.component'
+import { UserViewComponent } from './components/user-view/user-view.component'
+import { FilterViewComponent } from './components/filter-view/filter-view.component'
+import { MenuComponent } from './components/menu/menu.component'
 
 
 
