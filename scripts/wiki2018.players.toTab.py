3ca5cc14f7af67ba3478b69163d3751823285536
Ido Tamir <ido.tamir@vbcf.ac.at>
2018-05-27 16:58:18 +0200

    work on importer

--- scripts/wiki2018.players.toTab.py
+++ scripts/wiki2018.players.toTab.py
@@ -3,24 +3,73 @@
 
 import unittest
 import re
+import sys
+
+START="{{nat fs g start}}\n"
+END="{{nat fs g end}}\n"
+END2="{{nat fs end}}\n"
+BREAK="{{nat fs break}}\n"
+
+def parseCountry(infile):
+    players = []
+    table = False
+    for line in infile:
+        if line == END or line == END2:
+           return players
+        #print line
+        #print(line == START, START, line)
+        if line == START:
+           #print(line)
+           table = True
+           continue
+        if line == BREAK:
+           continue
+        if table:
+           player = parsePlayer(line)
+           players.append(player)
+
 
 def parseFile():
+    countryPlayers = {}
+    with open("wiki.fifa2018.squads.txt") as infile:
+         for line in infile:
+             m = re.search("===(\w*)===", line)
+        #     print(m, line)
+             if m is not None:
+                country = m.group(1)
+                players = parseCountry(infile)
+                countryPlayers[country] = players
+                continue
+    return countryPlayers
 
 def parsePlayer(line):
-    nm = re.search('name=\[\[(.*)\]\]\|sort', line)
-    name = nm.group(1)
-    name = re.sub("\(.*\)", "", name)
-    name = re.sub("\|", "", name)
-    #name=[[Mahmoud Hassan (footballer)|Trézéguet]]
-    sm = re.search('sortname=(\w*)\|',line)
-    sortname = sm.group(1)
-    pm = re.search('pos=(\w*)\|', line)
-    position = pm.group(1)
-    cm = re.search('club=\[\[(.*)\]\]\|club', line)
-    club = cm.group(1)
-    club = re.sub("\|.*","", club)
-    return (name, sortname, position, club)
+    try:
+        nm = re.search('name=\[\[(.*)\]\]\|sort', line)
+        name = nm.group(1)
+        name = re.sub("\(.*\)", "", name)
+        name = re.sub("\|", "", name)
+        #name=[[Mahmoud Hassan (footballer)|Trézéguet]]
+        sm = re.search('sortname=([a-zA-Z, _]*)',line)
+        sortname = sm.group(1)
+        pm = re.search('pos=(\w*)\|', line)
+        position = pm.group(1)
+        cm = re.search('club=\[\[(.*)\]\]\|club', line)
+        club = cm.group(1)
+        club = re.sub("\|.*","", club)
+        return (name, sortname, position, club)
+    except Exception as err:
+        print line
+        raise err
 
+def convert():
+    pC = parseFile()
+    with open("wiki.fifa2018.squads.tab", "w") as outf:
+        outf.write("country\tname\tsortname\tposition\tclub\n")
+        for country, players in pC.iteritems():
+            for p in players:
+                line = country+"\t"+"\t".join(p)+"\n"
+                outf.write(line)
+    #print(pC)
 
 class TestMethods(unittest.TestCase):
 
@@ -31,4 +80,5 @@ class TestMethods(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    #unittest.main()
+    convert()
