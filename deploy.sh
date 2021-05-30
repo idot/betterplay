c4a64a7cf330f90b3d5a41a3a87b3afe263587f2
Ido Tamir <ido.tamir@vbcf.ac.at>
2021-05-30 21:28:22 +0200

    EC2 deploy

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,12 @@
 #deploys the dist to the server. does not start it. 
 #log in on server and execute deploy_server.sh
 
-SERVER=better:/home/centos/better
-SERVER=solexa@gecko:/groups/vbcf-ngs/programs
+SERVER=${EC2_USER}@${EC2_INSTANCE}
 
 sbt dist
 if [ $? -eq 0 ]; then
-    scp target/universal/betterplay-0.9-SNAPSHOT.zip $SERVER
-    scp deploy_start.sh $SERVER
+    scp -v -i ${EC2_PEM} target/universal/betterplay-0.9-SNAPSHOT.zip ${SERVER}:~
+    scp -v -i ${EC2_PEM} deploy_start.sh $SERVER:~
 else
     echo "problem with sbt dist"
 fi
